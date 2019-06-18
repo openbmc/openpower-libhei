dcc8ab896d45a284b6b4d51c1608ed2fb2a48b36
Zane Shelley <zshelle@us.ibm.com>
2019-06-17 20:55:04 -0500

    Created initial README.md
    Change-Id: Ic799939d918b1b037ece1bc434a4e0abc5a39e97
    Signed-off-by: Zane Shelley <zshelle@us.ibm.com>

--- src/hei_isolator.cpp
+++ src/hei_isolator.cpp
@@ -6,22 +6,22 @@ namespace HEI
 
 Isolator::Isolator()
 {
-    HEI_TRAC( "Isolator::Isolator()" );
+    HEI_INF( "Isolator::Isolator()" );
 }
 
 Isolator::~Isolator()
 {
-    HEI_TRAC( "Isolator::~Isolator()" );
+    HEI_INF( "Isolator::~Isolator()" );
 }
 
 void Isolator::initialize()
 {
-    HEI_TRAC( "Isolator::initialize()" );
+    HEI_INF( "Isolator::initialize()" );
 }
 
 void Isolator::isolate()
 {
-    HEI_TRAC( "Isolator::isolate()" );
+    HEI_INF( "Isolator::isolate()" );
 }
 
 } // end namespace HEI
