d837eea566ad4d96c8152766d1c8eca06fb6138d
Paul Greenwood <Paul.Greenwood@ibm.com>
2019-07-03 08:58:38 -0500

    Added ChipDataStream class
    ChipDataStream makes it possible to read a buffer of binary data
    using the stream operators, ">>".
    
    Instantiate ChipDataStream class with a pointer to a data buffer
    and its size in bytes as below:
    
        ChipDataStream cds(streamData,552);
    
    Use the ">>" stream operator then to read the major C++ types
    (bool, char, uint8_t, int8_t, uint16_t, int16_t, uint32_t,
    int32_t, uint64_t, and int64_t).
    
    Signed-off-by: Paul Greenwood <Paul.Greenwood@ibm.com>
    Change-Id: I9927d96ca712d8027e8cbcae471b1e612f834bc1

---
+++ src/chip_data_stream.cpp
@@ -0,0 +1,98 @@
+#include "chip_data_stream.hpp"
+
+//Constructor
+ChipDataStream::ChipDataStream(uint8_t* i_buffer, size_t i_bufferSize)
+{
+    iv_buffer = i_buffer;
+    iv_bufferSize = i_bufferSize;
+    iv_asyncOffset =  0;
+}
+
+//Destructor
+ChipDataStream::~ChipDataStream(){};
+
+//Setters
+
+void ChipDataStream::setOffset(size_t i_offset)
+{
+    HEI_ASSERT(i_offset < iv_bufferSize);
+    iv_asyncOffset =  i_offset;
+}
+
+//Getters
+
+template <>
+    ChipDataStream & ChipDataStream::operator>>( bool & o_right )
+    {
+        read( &o_right, sizeof(o_right) );
+        return *this;
+    }
+
+template <>
+    ChipDataStream & ChipDataStream::operator>>( char & o_right )
+    {
+        read( &o_right, sizeof(o_right) );
+        return *this;
+    }
+
+template <>
+    ChipDataStream & ChipDataStream::operator>>( uint8_t & o_right )
+    {
+        read( &o_right, sizeof(o_right) );
+        return *this;
+    }
+
+template <>
+    ChipDataStream & ChipDataStream::operator>>( int8_t & o_right )
+    {
+        read( &o_right, sizeof(o_right) );
+        return *this;
+    }
+
+template <>
+    ChipDataStream & ChipDataStream::operator>>( uint16_t & o_right )
+    {
+        read( &o_right, 2);
+        be16toh( o_right );
+        return *this;
+    }
+
+template <>
+    ChipDataStream & ChipDataStream::operator>>( int16_t & o_right )
+    {
+        read( &o_right, sizeof(o_right) );
+        be16toh( o_right );
+        return *this;
+    }
+
+template <>
+    ChipDataStream & ChipDataStream::operator>>( uint32_t & o_right )
+    {
+        read( &o_right, sizeof(o_right) );
+        be32toh( o_right );
+        return *this;
+    }
+
+template <>
+    ChipDataStream & ChipDataStream::operator>>( int32_t & o_right )
+    {
+        read( &o_right, sizeof(o_right) );
+        be32toh( o_right );
+        return *this;
+    }
+
+template <>
+    ChipDataStream & ChipDataStream::operator>>( uint64_t & o_right )
+    {
+        read( &o_right, sizeof(o_right) );
+        be64toh( o_right );
+        return *this;
+    }
+
+template <>
+    ChipDataStream & ChipDataStream::operator>>( int64_t & o_right )
+    {
+        read( &o_right, sizeof(o_right) );
+        be64toh( o_right );
+        return *this;
+    }
