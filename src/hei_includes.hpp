5897a7facfc98078e0cb1fff5e84e38f70f9a04d
Zane Shelley <zshelle@us.ibm.com>
2019-07-23 12:55:37 -0500

    Created generic ReturnCode class and common includes header
    Change-Id: I2d294398d43d44596570faa56ce39ac69a9f0f50

---
+++ src/hei_includes.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+// The purpose of this file is to include common headers that will be used
+// throughout this library.
+
+#include <stdint.h>
+
+#include <hei_return_code.hpp>
+#include <hei_user_defines.hpp> // For HEI_ASSERT, HEI_INF, and HEI_ERR
+
