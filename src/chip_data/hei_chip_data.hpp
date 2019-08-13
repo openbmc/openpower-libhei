f85bb66c85bf5d671b6693296a9f68a40d94d218
Zane Shelley <zshelle@us.ibm.com>
2019-08-13 16:15:13 -0500

    Removed duplicate ChipType_t definitions
    Signed-off-by: Zane Shelley <zshelle@us.ibm.com>
    Change-Id: Idbe16f83dc6a519140bd8b489f0b3fe311fd48aa

--- src/chip_data/hei_chip_data.hpp
+++ src/chip_data/hei_chip_data.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
-#include "hei_includes.hpp"
+#include <hei_includes.hpp>
 
 namespace libhei
 {
 
 typedef uint32_t ChipType_t;
+
+static constexpr ChipType_t DEFAULT_CHIP_TYPE = 0;
 
 } // end namespace libhei
