2adb4f9b61298180871d855efb2d96d23066efb1
Paul Greenwood <Paul.Greenwood@ibm.com>
2019-08-15 19:29:31 -0500

    Added Register class.
    The Register class is a model of an actual physical register.
    The bits in the register are represented by the bit_string
    data member which is modified dynamically as operations are
    performed.  It acts as a temporarily cached value of the
    register.
    
    The current value of this cached bit string can be accessed
    or modified by other objects via the public interface.  The
    physical address and bit length of the hardware register are
    set during initialization and used on all accesses.
    
    Change-Id: I497d997c1cc338c53cb04a827bf2f4aca6b781c9
    Signed-off-by: Paul Greenwood <Paul.Greenwood@ibm.com>

--- src/hei_includes.hpp
+++ src/hei_includes.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
-// The purpose of this file is to include common headers that will be used
-// throughout this library.
+/**
+@file hei_includes.hpp
+@brief The purpose of this file is to include common headers that will be used
+throughout this library.
+*/
 
 // Standard library includes
 #include <stdint.h>
 
 // External includes
-#include <hei_user_defines.hpp> // For HEI_ASSERT, HEI_INF, and HEI_ERR
+#include <../test/hei_user_defines.hpp> // For HEI_ASSERT, HEI_INF, and HEI_ERR
 
 // Internal includes
 #include <hei_return_code.hpp>
