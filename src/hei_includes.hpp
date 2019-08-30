ca4b2f4b9f807be1f40bd7142923519ebf3652e5
Zane Shelley <zshelle@us.ibm.com>
2019-08-30 15:48:40 -0500

    Updated parameters to the user APIs
    Signed-off-by: Zane Shelley <zshelle@us.ibm.com>
    Change-Id: I9784c5044337c0f558a77d07f5cd4f5c2ef91e89

--- src/hei_includes.hpp
+++ src/hei_includes.hpp
@@ -1,17 +1,27 @@
 #pragma once
 
 /**
-@file hei_includes.hpp
-@brief The purpose of this file is to include common headers that will be used
-throughout this library.
-*/
+ * @file  hei_includes.hpp
+ * @brief The purpose of this file is to include common headers that will be
+ *        used throughout this library.
+ */
 
 // Standard library includes
+#include <stdlib.h>
 #include <stdint.h>
 #include <vector>
 
-// External includes
-#include <hei_user_defines.hpp> // For HEI_ASSERT, HEI_INF, and HEI_ERR
+// The user application must define "hei_user_defines.hpp" with the following
+// macros:
+//
+//   Tracing (inputs same as printf() from <cstdio>):
+//      HEI_INF( ... ) // Generic informational trace
+//      HEI_ERR( ... ) // Error path trace
+//
+//  Assertion (at a minimum should work like assert() from <cassert>):
+//      HEI_ASSERT( expression )
+//
+#include <hei_user_defines.hpp>
 
 // Internal includes
 #include <hei_chip.hpp>
