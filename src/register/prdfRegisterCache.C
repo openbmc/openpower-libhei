c5a7b5605eb21b752d23e4dcd4c664dcf1df5e88
Zane Shelley <zshelle@us.ibm.com>
2019-09-02 20:57:42 -0500

    Replaced NULL with nullptr in PRD ported code
    Signed-off-by: Zane Shelley <zshelle@us.ibm.com>
    Change-Id: Ic8b50b1437ba56189a4ea6814dd0b0f302bfdc1a

--- src/register/prdfRegisterCache.C
+++ src/register/prdfRegisterCache.C
@@ -52,7 +52,7 @@ BitString & RegDataCache::read( ExtensibleChip * i_chip,
     ScomRegisterAccess l_scomAccessKey ( *i_reg, i_chip );
     BitString * l_pBitString = queryCache( l_scomAccessKey );
 
-    if ( NULL == l_pBitString )
+    if ( nullptr == l_pBitString )
     {
         // Creating new entry
         l_pBitString = new BitStringBuffer( i_reg->GetBitLength() );
@@ -110,7 +110,7 @@ BitString * RegDataCache::queryCache(
 BitString * RegDataCache::queryCache(
                         const ScomRegisterAccess & i_scomAccessKey ) const
 {
-    BitString * l_pBitString = NULL;
+    BitString * l_pBitString = nullptr;
     CacheDump::const_iterator itDump = iv_cachedRead.find( i_scomAccessKey );
     if( iv_cachedRead.end() != itDump )
     {
