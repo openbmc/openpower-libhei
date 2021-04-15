552992a66a275a046a93922dc14fa551da5a7f05
Ben Tyner <ben.tyner@ibm.com>
2021-04-15 10:57:17 -0500

    Add build information to binary
    Build information will be compiled into the binary. An associated
    function was added to retrieve this information for displaying in
    applications that consume this library.
    
    Signed-off-by: Ben Tyner <ben.tyner@ibm.com>
    Change-Id: I68e467c714ea11c0f5639670396617bc0ff51334

--- src/isolator/hei_isolator.cpp
+++ src/isolator/hei_isolator.cpp
@@ -1,4 +1,5 @@
 
+#include <buildinfo.hpp>
 #include <isolator/hei_isolation_node.hpp>
 #include <isolator/hei_isolator.hpp>
 #include <register/hei_operator_register.hpp>
@@ -71,6 +72,11 @@ void Isolator::isolate(const std::vector<Chip>& i_chipList,
         // Analyze this chip.
         itr->second->analyze(chip, o_isoData);
     }
+}
+
+const char* getBuildInfo()
+{
+    return BUILDINFO;
 }
 
 } // end namespace libhei
